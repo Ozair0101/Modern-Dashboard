7d2c40468e3fef535752cf891d7de0e5b107fc01
Ozair0101 <ozairjan186@gmail.com>
2025-10-09 18:48:44 +0430

    test

---
+++ src/components/portfolio/TestHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestHome = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Home Component is Working!</h1>
+    </div>
+  );
+};
+
+export default TestHome;
